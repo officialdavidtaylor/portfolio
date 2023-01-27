3eec3945baae0084f1d12b2069eacc1dc7ff3121
officialdavidtaylor <officialdavidtaylor@gmail.com>
2023-01-26 17:51:43 -0800

    init: instal pkgs and remove boilerplate

---
+++ env.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NOTION_AUTH_TOKEN: z.string(),
+  NOTION_PROJECTS_DATABASE_ID: z.string(),
+});
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> {}
+  }
+}
