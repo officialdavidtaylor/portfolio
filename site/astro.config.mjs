a2eafa9e5ccd4ab5a4ce5b4106b85d970f4a5491
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-02-19 16:10:31 -0800

    feat: integrate Sanity client with Astro site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import { sanityIntegration } from "@sanity/astro";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
+
+const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  integrations: [
+    sanityIntegration({
+      projectId: SANITY_PROJECT_ID,
+      dataset: SANITY_DATASET,
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    }),
+    tailwind(),
+  ],
   adapter: netlify(),
 });
--- site/src/env.d.ts
+++ site/src/env.d.ts
@@ -1,6 +1,13 @@
 /// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
 
 type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals;
+
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
 declare namespace App {
   interface Locals extends NetlifyLocals {}
